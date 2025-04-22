3d892bda08a5afbe49fd156990bf7f830b8862bf
Janhavi Doijad <janhavidoijad@gmail.com>
2025-04-22 09:08:40 +0530

    Modified DatabaseException

--- src/main/java/com/sawal/exceptions/DatabaseException.java
+++ src/main/java/com/sawal/exceptions/DatabaseException.java
@@ -1,14 +1,31 @@
 // DatabaseException.java
 package com.sawal.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import java.util.Map;
+
 /**
- * Thrown when there is any problem connecting to or querying the database.
+ * Thrown for any SQL execution or database connection errors.
  */
-public class DatabaseException extends RuntimeException {
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+public class DatabaseException extends QueryException {
+    private static final long serialVersionUID = 3L;
+    private static final String CODE = "DB_ERROR";
+
     public DatabaseException(String message) {
-        super(message);
+        super(CODE, message, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
     public DatabaseException(String message, Throwable cause) {
-        super(message, cause);
+        super(CODE, message, cause, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    public DatabaseException(String message, Map<String, Object> details) {
+        super(CODE, message, HttpStatus.INTERNAL_SERVER_ERROR, details);
+    }
+
+    public DatabaseException(String message, Throwable cause, Map<String, Object> details) {
+        super(CODE, message, cause, HttpStatus.INTERNAL_SERVER_ERROR, details);
     }
 }
