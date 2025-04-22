f70e158aed302904207cece9c1e2488890a20391
Janhavi Doijad <janhavidoijad@gmail.com>
2025-04-22 09:07:10 +0530

    Modified APIException

--- src/main/java/com/sawal/exceptions/APIException.java
+++ src/main/java/com/sawal/exceptions/APIException.java
@@ -1,14 +1,31 @@
 // APIException.java
 package com.sawal.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import java.util.Map;
+
 /**
- * A generic runtime exception for wrapping any API‐layer errors.
+ * Thrown for failures in external API communication or response parsing.
  */
-public class APIException extends RuntimeException {
+@ResponseStatus(HttpStatus.BAD_GATEWAY)
+public class APIException extends QueryException {
+    private static final long serialVersionUID = 2L;
+    private static final String CODE = "API_ERROR";
+
     public APIException(String message) {
-        super(message);
+        super(CODE, message, HttpStatus.BAD_GATEWAY);
     }
+
     public APIException(String message, Throwable cause) {
-        super(message, cause);
+        super(CODE, message, cause, HttpStatus.BAD_GATEWAY);
+    }
+
+    public APIException(String message, Map<String, Object> details) {
+        super(CODE, message, HttpStatus.BAD_GATEWAY, details);
+    }
+
+    public APIException(String message, Throwable cause, Map<String, Object> details) {
+        super(CODE, message, cause, HttpStatus.BAD_GATEWAY, details);
     }
 }
